50ba28d5a0551e8da7097b2997f77bd28618073b
AMIT MISHRA <79392743+iamyoursonly@users.noreply.github.com>
2025-03-11 21:05:25 +0530

    Update socket.js

--- frontend/src/services/socket.js
+++ frontend/src/services/socket.js
@@ -1,5 +1,5 @@
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:5000"); // Backend URL
+const socket = io("https://chatbot-1-f96n.onrender.com"); // Backend URL
 
 export default socket;
