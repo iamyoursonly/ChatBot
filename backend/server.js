29a19f5bfebccebb9daffdc7e2a5bdb83f6a2073
AMIT MISHRA <79392743+iamyoursonly@users.noreply.github.com>
2025-03-15 12:08:48 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,7 @@ const io = new Server(server, {
   },
 });
 
-app.use(cors());
+app.use(cors({ origin: "*" })); 
 app.use(express.json());
 app.get("/", (req, res) => {
     res.send("Backend is running smoothly!");
